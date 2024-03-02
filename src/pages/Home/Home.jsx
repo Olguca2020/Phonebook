940b8ff09769d8df17e797903115dc6e39b20c87
Shchyrba Olga <olga.binder@gmail.com>
2024-03-02 09:45:08 +0200

    register2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import css from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className={css.container}>
-      <img src={myImg} alt="Описание изображения" className={css.image} />
+    <div className={css.homeContainer}>
+      <img src={myImg} alt="Фото собаки с телефоном" className={css.image} />
       <div className={css.textContainer}>
         <h1 className={css.fadeInText}>PHONEBOOK</h1>
       </div>
