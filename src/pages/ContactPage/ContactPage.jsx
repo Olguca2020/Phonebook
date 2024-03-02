002da8d1b56411b558021c6ba68e6a6e63fc547b
Shchyrba Olga <olga.binder@gmail.com>
2024-03-02 17:25:16 +0200

    false

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,15 +1,17 @@
 import css from "./ContactPage.module.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import SearchBox from "../../components/SearchBox/SearchBox";
+
 import SectionContactForm from "../../components/SectionContactForm/SectionContactForm";
-import Loader from "../../components/Loader/Loader";
+
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import { fetchContacts } from "../../redux/contacts/operations";
 import { selectIsLoading } from "../../redux/contacts/selectors";
 import { selectError } from "../../redux/contacts/selectors";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { ContactList } from "../../components/ContactList/ContactList";
+import { SearchBox } from "../../components/SearchBox/SearchBox";
+import { MyLoader } from "../../components/Loader/Loader";
 
 export default function ContactPage() {
   const dispatch = useDispatch();
@@ -31,7 +33,7 @@ export default function ContactPage() {
         <div className={css.wrapContacts}>
           <SearchBox />
           <ContactList />
-          {isLoading && <Loader />}
+          {isLoading && <MyLoader />}
           {error && <ErrorMessage />}
         </div>
       </HelmetProvider>
