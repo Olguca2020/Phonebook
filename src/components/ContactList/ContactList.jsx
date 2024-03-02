4998141ee93bbd3639e13a22c047bd646be69ad7
Shchyrba Olga <olga.binder@gmail.com>
2024-03-02 18:05:52 +0200

    5

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,8 +2,8 @@ import { Contact } from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
-import { deleteCard } from "../../redux/operation";
 import { selectVisibleCard } from "../../redux/selectors";
+import { deleteCard } from "../../redux/handleCards/operation";
 
 export const ContactList = () => {
   const selectCard = useSelector(selectVisibleCard);
