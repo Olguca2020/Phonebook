bcb4ac7e15d140bc95d2d302d883e8f388965f57
Shchyrba Olga <olga.binder@gmail.com>
2024-03-03 09:24:44 +0200

    path of useAuth

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./hooks";
+import { useAuth } from "./hooks/useAuth";
 
 /**
  * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import css from "./UserMenu.module.css";
 import { logOut } from "../../redux/auth/operationAuth";
-import { useAuth } from "../hooks";
+import { useAuth } from "../hooks/useAuth";
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
